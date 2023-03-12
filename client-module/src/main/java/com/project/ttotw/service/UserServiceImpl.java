a8643fc859647be27817eb852af93054c10358c0
JianChoi-Kor <wldks79@gmail.com>
2023-03-13 00:07:37 +0900

    (client-module) jwt reissue

--- client-module/src/main/java/com/project/ttotw/service/UserServiceImpl.java
+++ client-module/src/main/java/com/project/ttotw/service/UserServiceImpl.java
@@ -54,7 +54,6 @@ public class UserServiceImpl implements UserService {
     }
 
     public ResponseEntity<?> reissue(HttpServletRequest request) {
-
         //TODO:: 1, 2 는 JwtAuthenticationFilter 동작과 중복되는 부분, 때문에 jwt filter 에서 다른 key 값으로 refresh token 값을
         //넘겨주고 여기서 받아서 처리하는 방법도 적용해 볼 수 있을 듯
 
